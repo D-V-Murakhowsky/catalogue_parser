6365a4b9ea13efc171f524707adc7a15009fac9f
DenysMurakhovskyi <dmurakhovskyi@protonmail.com>
2022-11-03 22:31:13 +0200

    tests fixes

--- tests/func/selenium/test_several_pages_getter.py
+++ tests/func/selenium/test_several_pages_getter.py
@@ -6,5 +6,5 @@ from application.selenium_connector.page_getter import PageGetter
 class SeveralPagesParsing(TestCase):
 
     def test_several_pages_parsing(self):
-        res = PageGetter._get_pages(1, 4)
+        res = PageGetter._get_pages(1, 3)
         self.assertEqual(3, len(res))
