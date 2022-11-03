5b3a6ea165cdd3f368f28f72d0282f6e488ea67a
DenysMurakhovskyi <dmurakhovskyi@protonmail.com>
2022-11-03 19:11:44 +0200

    getting GSheet into dataframe with filtering

--- application/google_connector.py
+++ application/google_connector.py
@@ -10,15 +10,16 @@ class GoogleConnector(metaclass=Singleton):
 
     def __init__(self):
         try:
-            self.client = pgs.authorize(client_secret=self._get_credentials_path())
+            self.client = pgs.authorize(client_secret=self._get_credentials_dir() / 'client_secret.json',
+                                        credentials_directory=self._get_credentials_dir())
             self.sh = self.client.open(config.google_table_name)
             self.ws = self.sh.worksheet('title', config.sheet_to_sync_name)
         except Exception as ex:
             pass
 
     @staticmethod
-    def _get_credentials_path() -> pathlib.Path:
-        return pathlib.Path(__file__).parents[1].resolve() / 'assets/client_secret.json'
+    def _get_credentials_dir() -> pathlib.Path:
+        return pathlib.Path(__file__).parents[1].resolve() / 'assets'
 
     def get_table_into_df(self) -> pd.DataFrame:
         df = self.ws.get_as_df(end=f'AF{self.ws.rows}', numerize=False)
