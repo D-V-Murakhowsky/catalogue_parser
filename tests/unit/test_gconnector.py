3c41c60258d470db887c2fe34386b4c05c2bcc5a
DenysMurakhovskyi <dmurakhovskyi@protonmail.com>
2022-11-29 22:47:36 +0200

    redundant tests deletion

--- tests/unit/test_gconnector.py
+++ tests/unit/test_gconnector.py
@@ -7,13 +7,6 @@ from application.google_connector import GoogleConnector
 
 class TestGConnector(TestCase):
 
-    def setUp(self) -> None:
-        self.gc = GoogleConnector()
-
-    def test_get_table_into_df(self):
-        actual = self.gc.get_table_into_df()
-        self.assertIsNotNone(actual)
-
     def test_get_numeric_code(self):
         sequence = ['SP4309', 'SP_4309', 'SP_4320-100', 'SP-4320-100']
         actual = list(map(GoogleConnector._get_the_numeric_code, sequence))
